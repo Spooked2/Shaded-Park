df96aaf49cde69ba61c27c92b7dc7dc50d6f64f0
Sander Landmeter <sander@click.nl>
2026-03-23 15:52:00 +0100

    ADD basic player movement and collision

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,1 +1,21 @@
-extends Node3D
+extends CharacterBody3D
+
+var speed: float = 1.65
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	var input_direction: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction: Vector3 = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	move_and_slide()
